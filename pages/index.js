31fd6c43b0df5afbd7a0021b266d8afd3c1b5a27
Matt Lim <mlimcollege@gmail.com>
2021-06-11 21:50:18 -0700

    Switch to alpha version of react and react-dom, which lets Suspense be called during SSR

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import RelayEnvironment from "../src/RelayEnvironment";
 import { RelayEnvironmentProvider } from "react-relay";
+import RelayTest from "../src/RelayTest.js";
 import RelayTestDynamic from "../src/RelayTestDynamic.js";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
-      <RelayTestDynamic />
+      {/* <RelayTestDynamic /> */}
+      <RelayTest />
     </RelayEnvironmentProvider>
   );
 }
